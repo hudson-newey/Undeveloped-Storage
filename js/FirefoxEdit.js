fd8b9fa11b0db12721228c45a21ca1b8678e8c5f
Grathium Industries <grathiumsoftwears@gmail.com>
2020-04-21 18:36:22 +1000

    Update FirefoxEdit.js

--- js/FirefoxEdit.js
+++ js/FirefoxEdit.js
@@ -2,15 +2,7 @@
 // @name         Firefox Edit
 // @include      *://*
 // ==/UserScript==
-
-(function() {
-    'use strict';
-    document.getElementsByTagName("video").setAttribute("loop", "");
-})();
-
-document.onmouseup = function(){
+document.onmouseup=function(){
     var temps = window.getSelection();
-    if (temps == "" || temps == null) {
-        document.activeElement.blur();
-    }
+    if(temps==""||temps==null){document.activeElement.blur();}
 };
