869d8455303417458b274057ffd408cf0a3d6fb7
Grathium Industries <GrathiumIndustries@gmail.com>
2020-06-23 14:02:43 +1000

    F# development

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿open System;
+
+// custom input function w/ prompt
+let input (prompt : string) : string =
+    printf "%s" prompt
+    Console.ReadLine()
+
+[<EntryPoint>]
+let main argv =
+    let mutable total = 1
+    for i in total..(input("Count to:  ") |> int) do
+        total <- total * i
+    printf "Factorial is: %i" total
+
+    // END
+    Console.ReadKey() |> ignore
+    0
